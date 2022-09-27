4f124648cc25ae503fbfc2bfbef852624e840609
lawsk <113019133+lawsk@users.noreply.github.com>
2022-09-27 19:16:27 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "edgerunner.c0rjyw0pzejy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "edgerunner"
+custombucket = "lawshiekkiong-bucket"
+customregion = "us-east-1"
